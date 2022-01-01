95bafb84c89b41d90bc3ad582f15c0efea086781
konhi <hello.konhi@gmail.com>
2022-01-01 01:48:07 +0100

    feat(most_common_words): :sparkles: anki support

--- scripts/decks/most_common_words/anki.py
+++ scripts/decks/most_common_words/anki.py
@@ -1,6 +1,7 @@
-import utils
+import genanki
 
-from main import __version__, __dataset__
+import main
+import utils
 
 """Return static id for a deck. It gives ability to update deck in Anki.
 All below doesn't really matter, it just makes decks updatable.
@@ -11,7 +12,7 @@ Every id starts with 1 to prevent trimming zeros.
 - 1 de (32) pl (128) -> 1 032 128 000
 - 1 de (32) pl (128) das, ten (1) -> 1 032 128 001
 """
-def get_specific_id(questions_language, answers_language, word_index = 0):
+def get_specific_id(questions_language: str, answers_language: str, word_index: int = 0):
   GREATEST_SHORTCODE_INDEX_LENGTH = 3
   REQUIRED_DECK_ID_LENGTH = 10
   FILL_WITH = '0'
@@ -24,4 +25,35 @@ def get_specific_id(questions_language, answers_language, word_index = 0):
 
 
 def get_package_name(question_language, answers_language):
-  return f'frekwencja: {__dataset__} v{__version__} {question_language} - {answers_language}'
+  return f'frekwencja: {main.__dataset__} v{main.__version__} {question_language} - {answers_language}.apkg'
+
+
+def get_audio_path(language: str, word: str):
+  return f'data/pronunciation/{language}/frekwencja_{language}_{word}.mp3'
+
+
+def get_deck(question_language, answer_language):
+  deck = genanki.Deck(get_specific_id(question_language, answer_language), get_package_name(question_language, answer_language))
+  media_files = []
+
+  for i, word in enumerate(main.wordpairs):
+    question = main.wordpairs[word][question_language]
+    answer = main.wordpairs[word][answer_language]
+
+    path = get_audio_path(answer_language, answer)
+
+    filename = path.split('/')[-1]
+    media_files.append(path)
+
+    note = genanki.Note(main.MODEL, fields=[question, answer, f'[sound:{filename}]'])
+
+    deck.add_note(note)
+
+    if i + 1 == main.MAX_WORDS:
+      break
+
+  print(media_files)
+  return genanki.Package(deck, media_files)
+
+
+get_deck('pl', 'de').write_to_file(get_package_name('pl', 'de'))
--- scripts/decks/most_common_words/main.py
+++ scripts/decks/most_common_words/main.py
@@ -1,11 +1,31 @@
 import requests
 from typing import Dict
+import genanki
 
 __version__ = '0.1.0'
 __dataset__ = 'Most Common Words'
 
+MAX_WORDS = 1000
 DEFAULT_URL = 'https://github.com/frekwencja/most-common-words-multilingual/releases/download/0.1.0/all.json'
+MODEL = genanki.Model(
+  2000000000,
+  'Frekwencja Basic Model',
+  fields=[
+    {'name': 'Question'},
+    {'name': 'Answer'},
+    {'name': 'Pronunciation'}
+  ],
+  templates=[
+    {
+      'name': 'Card 1',
+      'qfmt': '{{Question}} {{Pronunciation}}',
+      'afmt': '{{Answer}}',
+    },
+  ])
 
 """Return object of english words as key and `language: translation` as value. Check the link to know the schema. Shortcodes are in ISO-639-1 standard."""
 def get_multilingual_wordpairs(url: str = DEFAULT_URL) -> Dict[str, Dict[str, str]]:
   return requests.get(url).json()
+
+
+wordpairs = get_multilingual_wordpairs('http://localhost:2137/all.json')
